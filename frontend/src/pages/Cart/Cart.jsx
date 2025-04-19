5e61456b43b862d440d7438e5f56114fb4754b6f
Saud0786 <165356306+Saud0786@users.noreply.github.com>
2025-04-19 15:06:01 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -59,7 +59,7 @@ function Cart() {
               <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button onClick={()=>navigate('/card')} >PROCEED TO PAYMENT</button>
+          <button onClick={()=>navigate('/')} >PROCEED TO PAYMENT</button>
         </div>
 
         <div class="cart-promocode">
